16f8a12ebaea0f07bc807da99406d88ae763523d
MathInDOS <71683721+MathInDOS@users.noreply.github.com>
2020-10-10 13:18:11 +0530

    Add files via upload

---
+++ Simple Encryption Source Code/Simple Encryption/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEncryption
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmEncryption))
+        Me.txtMessage = New System.Windows.Forms.TextBox
+        Me.txtEncoded = New System.Windows.Forms.TextBox
+        Me.cmdCreate = New System.Windows.Forms.Button
+        Me.cmdLoad = New System.Windows.Forms.Button
+        Me.cmdEncrypt = New System.Windows.Forms.Button
+        Me.cmdDecrypt = New System.Windows.Forms.Button
+        Me.cmdSave = New System.Windows.Forms.Button
+        Me.cmdCancel = New System.Windows.Forms.Button
+        Me.cmdExit = New System.Windows.Forms.Button
+        Me.sfdEncFile = New System.Windows.Forms.SaveFileDialog
+        Me.ofdEncFile = New System.Windows.Forms.OpenFileDialog
+        Me.SuspendLayout()
+        '
+        'txtMessage
+        '
+        Me.txtMessage.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.txtMessage.Location = New System.Drawing.Point(10, 10)
+        Me.txtMessage.Multiline = True
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.Size = New System.Drawing.Size(600, 200)
+        Me.txtMessage.TabIndex = 0
+        '
+        'txtEncoded
+        '
+        Me.txtEncoded.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.txtEncoded.Location = New System.Drawing.Point(10, 240)
+        Me.txtEncoded.Multiline = True
+        Me.txtEncoded.Name = "txtEncoded"
+        Me.txtEncoded.Size = New System.Drawing.Size(600, 200)
+        Me.txtEncoded.TabIndex = 1
+        '
+        'cmdCreate
+        '
+        Me.cmdCreate.Location = New System.Drawing.Point(10, 450)
+        Me.cmdCreate.Name = "cmdCreate"
+        Me.cmdCreate.Size = New System.Drawing.Size(100, 23)
+        Me.cmdCreate.TabIndex = 2
+        Me.cmdCreate.Text = "Create Message"
+        Me.cmdCreate.UseVisualStyleBackColor = True
+        '
+        'cmdLoad
+        '
+        Me.cmdLoad.Location = New System.Drawing.Point(133, 450)
+        Me.cmdLoad.Name = "cmdLoad"
+        Me.cmdLoad.Size = New System.Drawing.Size(100, 23)
+        Me.cmdLoad.TabIndex = 3
+        Me.cmdLoad.Text = "Load File"
+        Me.cmdLoad.UseVisualStyleBackColor = True
+        '
+        'cmdEncrypt
+        '
+        Me.cmdEncrypt.Location = New System.Drawing.Point(10, 479)
+        Me.cmdEncrypt.Name = "cmdEncrypt"
+        Me.cmdEncrypt.Size = New System.Drawing.Size(100, 23)
+        Me.cmdEncrypt.TabIndex = 4
+        Me.cmdEncrypt.Text = "Encrypt"
+        Me.cmdEncrypt.UseVisualStyleBackColor = True
+        '
+        'cmdDecrypt
+        '
+        Me.cmdDecrypt.Location = New System.Drawing.Point(133, 479)
+        Me.cmdDecrypt.Name = "cmdDecrypt"
+        Me.cmdDecrypt.Size = New System.Drawing.Size(100, 23)
+        Me.cmdDecrypt.TabIndex = 5
+        Me.cmdDecrypt.Text = "Decrypt"
+        Me.cmdDecrypt.UseVisualStyleBackColor = True
+        '
+        'cmdSave
+        '
+        Me.cmdSave.Location = New System.Drawing.Point(10, 508)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(100, 23)
+        Me.cmdSave.TabIndex = 6
+        Me.cmdSave.Text = "Save to File"
+        Me.cmdSave.UseVisualStyleBackColor = True
+        '
+        'cmdCancel
+        '
+        Me.cmdCancel.Location = New System.Drawing.Point(133, 508)
+        Me.cmdCancel.Name = "cmdCancel"
+        Me.cmdCancel.Size = New System.Drawing.Size(100, 23)
+        Me.cmdCancel.TabIndex = 7
+        Me.cmdCancel.Text = "Cancel"
+        Me.cmdCancel.UseVisualStyleBackColor = True
+        '
+        'cmdExit
+        '
+        Me.cmdExit.ForeColor = System.Drawing.Color.Red
+        Me.cmdExit.Location = New System.Drawing.Point(535, 508)
+        Me.cmdExit.Name = "cmdExit"
+        Me.cmdExit.Size = New System.Drawing.Size(75, 23)
+        Me.cmdExit.TabIndex = 8
+        Me.cmdExit.Text = "Exit"
+        Me.cmdExit.UseVisualStyleBackColor = True
+        '
+        'ofdEncFile
+        '
+        Me.ofdEncFile.FileName = "OpenFileDialog1"
+        '
+        'frmEncryption
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(624, 542)
+        Me.Controls.Add(Me.cmdExit)
+        Me.Controls.Add(Me.cmdCancel)
+        Me.Controls.Add(Me.cmdSave)
+        Me.Controls.Add(Me.cmdDecrypt)
+        Me.Controls.Add(Me.cmdEncrypt)
+        Me.Controls.Add(Me.cmdLoad)
+        Me.Controls.Add(Me.cmdCreate)
+        Me.Controls.Add(Me.txtEncoded)
+        Me.Controls.Add(Me.txtMessage)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmEncryption"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Simple Encryptor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtMessage As System.Windows.Forms.TextBox
+    Friend WithEvents txtEncoded As System.Windows.Forms.TextBox
+    Friend WithEvents cmdCreate As System.Windows.Forms.Button
+    Friend WithEvents cmdLoad As System.Windows.Forms.Button
+    Friend WithEvents cmdEncrypt As System.Windows.Forms.Button
+    Friend WithEvents cmdDecrypt As System.Windows.Forms.Button
+    Friend WithEvents cmdSave As System.Windows.Forms.Button
+    Friend WithEvents cmdCancel As System.Windows.Forms.Button
+    Friend WithEvents cmdExit As System.Windows.Forms.Button
+    Friend WithEvents sfdEncFile As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents ofdEncFile As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ Simple Encryption Source Code/Simple Encryption/Form1.vb
@@ -0,0 +1,132 @@
+﻿
+
+Public Class frmEncryption
+
+    Dim intMaxLength As Integer = 1024
+    Dim chrCodeKeyArray(255) As Char
+    Dim strKeyFileName As String
+    Dim boolMsgFlag As Boolean = False
+
+    Private Sub frmEncryption_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MaximizeBox = False
+        txtMessage.MaxLength = intMaxLength
+        cmdCreate.Focus()
+    End Sub
+
+    Private Sub cmdCreate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCreate.Click
+        txtMessage.ReadOnly = False
+        txtMessage.BackColor = Color.White
+        txtMessage.Clear()
+        txtEncoded.Clear()
+        txtMessage.Focus()
+        cmdLoad.Enabled = False
+        cmdCreate.Enabled = False
+        cmdCancel.Enabled = True
+
+    End Sub
+
+    Private Sub txtMessage_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtMessage.TextChanged
+        If Len(txtMessage.Text) > 0 And txtMessage.ReadOnly = False Then
+            If boolMsgFlag = True Then
+                cmdEncrypt.Enabled = True
+            End If
+
+        Else
+            boolMsgFlag = False
+            cmdEncrypt.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub Clear()
+        txtMessage.Clear()
+        txtEncoded.Clear()
+        For i = 0 To Len(chrCodeKeyArray) - 1
+            chrCodeKeyArray(i) = ""
+        Next
+        cmdDecrypt.Enabled = False
+        cmdCancel.Enabled = False
+        txtMessage.ReadOnly = True
+        txtMessage.BackColor = SystemColors.ControlLight
+        cmdSave.Enabled = False
+        cmdCreate.Enabled = True
+        cmdLoad.Enabled = True
+        cmdCreate.Focus()
+    End Sub
+
+
+    Private Sub cmdCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCancel.Click
+        Clear()
+
+    End Sub
+    Private Function Encode(ByVal InputChar As Char) As Char
+        Dim EncodedChar As Char
+
+        If chrCodeKeyArray(Asc(InputChar)) = Nothing Then
+            Randomize()
+            Do
+                EncodedChar = Chr(CInt(255 * Rnd()))
+            Loop Until InStr(CStr(chrCodeKeyArray), EncodedChar) = 0
+            chrCodeKeyArray(Asc(InputChar)) = EncodedChar
+            Return EncodedChar
+        Else
+            Return chrCodeKeyArray(Asc(InputChar))
+        End If
+    End Function
+
+    Private Sub cmdEncrypt_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdEncrypt.Click
+        Dim strTemp As String = ""
+
+        For i = 0 To Len(txtMessage.Text) - 1
+            If Asc(txtMessage.Text(i)) > 126 Or Asc(txtMessage.Text(i)) < 32 Then
+                strTemp &= Encode(" ")
+            Else
+                strTemp &= Encode(txtMessage.Text(i))
+            End If
+        Next
+        txtEncoded.Text = strTemp
+        cmdEncrypt.Enabled = False
+        txtMessage.ReadOnly = True
+        txtMessage.BackColor = SystemColors.ControlLight
+        cmdSave.Enabled = True
+    End Sub
+
+    Private Sub cmdSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSave.Click
+        sfdEncFile.Filter = "Encrypted Files|*.enc"
+        If sfdEncFile.ShowDialog = DialogResult.OK Then
+            My.Computer.FileSystem.WriteAllText(sfdEncFile.FileName, txtEncoded.Text, False)
+            strKeyFileName = Mid(sfdEncFile.FileName, 1, Len(sfdEncFile.FileName) - 4) & ".key"
+            My.Computer.FileSystem.WriteAllText(strKeyFileName, chrCodeKeyArray, False)
+            Clear()
+        End If
+    End Sub
+
+    Private Sub cmdLoad_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdLoad.Click
+        ofdEncFile.Filter = "Encrypted Files|*.enc"
+        If ofdEncFile.ShowDialog = DialogResult.OK Then
+            txtEncoded.Text = My.Computer.FileSystem.ReadAllText(ofdEncFile.FileName)
+            txtMessage.Clear()
+            strKeyFileName = Mid(ofdEncFile.FileName, 1, _
+              Len(ofdEncFile.FileName) - 4) & ".key"
+            chrCodeKeyArray = My.Computer.FileSystem.ReadAllText(strKeyFileName)
+            cmdCancel.Enabled = True
+            cmdDecrypt.Enabled = True
+            cmdCreate.Enabled = False
+            cmdLoad.Enabled = False
+        End If
+    End Sub
+
+    Private Sub cmdDecrypt_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdDecrypt.Click
+        For i = 0 To Len(txtEncoded.Text) - 1
+            txtMessage.Text &= Chr(InStr(chrCodeKeyArray, txtEncoded.Text(i)) - 1)
+        Next
+        cmdCancel.Enabled = False
+        cmdDecrypt.Enabled = False
+        cmdCreate.Enabled = True
+        cmdLoad.Enabled = True
+    End Sub
+
+    Private Sub cmdExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdExit.Click
+        End
+    End Sub
+End Class
